3123d8545ae154439d0b3a2f3bf4adf8bb54f275
maathangi96 <maathangi96@gmail.com>
2017-06-16 09:57:17 +0530

    abcd.java

---
+++ abcd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class abcd
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+double a=s.nextDouble();
+double b=s.nextDouble();
+double c=Math.pow(a,b);
+System.out.println(c);
+}
+}

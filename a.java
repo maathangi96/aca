761df6a473c627f1892f64b45504b5eb16ea647e
maathangi96 <maathangi96@gmail.com>
2017-06-16 09:16:11 +0530

    a.java

---
+++ a.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class a
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int N=s.nextInt();
+int sum=0;
+for(int i=0;i<=N;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
